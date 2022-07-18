a3ddc3cf3cd25f63a308daa963d1d222a28fa4df
Hyeonmani <hyeonmani@naver.com>
2022-07-18 23:52:05 +0900

    반복문과 조건문

---
+++ ch04/FlowEx22.java
@@ -0,0 +1,17 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class FlowEx22 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for(int tmp : arr){
+            System.out.printf("%d ", tmp);
+            sum += tmp;
+        }
+        System.out.println();
+        System.out.println("sum = " + sum);
+    }
+}
